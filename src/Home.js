c8e2dfd64c89d84bd53db6e21ae7414684307b89
Katelyn <katelynjewel@gmail.com>
2022-03-31 15:59:17 -0700

    made react hook useFetch

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,14 @@
-import { useState, useEffect } from 'react'; 
 import BlogDetails from './BlogDetails';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then((data) => {
-        console.log(data)
-      })
-  }, []);
+  const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
   return ( 
     <div className="home">
-      {/* <BlogDetails blogs={blogs} handleDelete={handleDelete} title="All Blogs!"/> */}
+      { error && <div>{ error }</div>}
+      { isLoading && <div>Loading...</div>}
+      {blogs && <BlogDetails blogs={blogs} title="All Blogs!"/>}
     </div>
   );
 }
