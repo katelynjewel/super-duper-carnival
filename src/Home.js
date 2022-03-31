a40180b193236b81e4df15a17af9a34f670cd775
Katelyn <katelynjewel@gmail.com>
2022-03-31 14:28:18 -0700

    dog blog

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
 import { useState } from 'react'; 
 
 const Home = () => {
-  const [name, setName] = useState('winnie');
-  const [age, setAge] = useState(15);
-
-  const handleClick = () => {
-    setName('winston');
-    setAge(25);
-  }
+  const [blogs, setblogs] = useState([
+    { title: 'Friend or Best Friend?', body: 'lorem ipsum...', author: 'Winston', id: 1 },
+    { title: 'Toy party!', body: 'lorem ipsum...', author: 'Teegan', id: 2 },
+    { title: 'Beggine for more food tips', body: 'lorem ipsum...', author: 'Dave', id: 3 }
+  ]);
 
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p> Written by: { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
